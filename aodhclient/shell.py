479bbc2c206cecabd88f88c1ec16630feae4eb59
ZhiQiang Fan <aji.zqfan@gmail.com>
2016-05-02 22:41:18 +0800

    fix KeyError when credential is invalid
    We assume the keystoneauth1.exceptions.HttpError.response has
    a field named `description`, however, it doesn't.[1]
    
    We should use `details` instead. [2]
    
    [1]. https://github.com/openstack/keystoneauth/blob/2.6.0/keystoneauth1/exceptions/http.py#L65-L85
    [2]. https://github.com/openstack/keystoneauth/blob/2.6.0/keystoneauth1/exceptions/http.py#L391
    
    Change-Id: I77846c6cafc0f73a1c6cf20a17fe44ba1284eef1
    Closes-Bug: #1577290

--- aodhclient/shell.py
+++ aodhclient/shell.py
@@ -13,6 +13,8 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
+from __future__ import print_function
+
 import logging
 import os
 import sys
@@ -129,13 +131,8 @@ class AodhShell(app.App):
         return self._client
 
     def clean_up(self, cmd, result, err):
-        if err and isinstance(err, exceptions.HttpError):
-            try:
-                error = err.response.json()
-            except Exception:
-                pass
-            else:
-                print(error['description'])
+        if isinstance(err, exceptions.HttpError) and err.details:
+            print(err.details, file=sys.stderr)
 
     def configure_logging(self):
         if self.options.debug:
