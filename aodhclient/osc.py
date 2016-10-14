7f0ce5424988960a2b727047a74e5093aec6171e
KATO Tomoyuki <kato.tomoyuki@jp.fujitsu.com>
2016-10-14 13:56:13 +0900

    Add plug-in summary for osc doc
    Stevedore Sphinx extension handles this comment.
    http://docs.openstack.org/developer/python-openstackclient/plugin-commands.html
    
    Change-Id: Id999b32cf00a79b1344a8fbaff55f8bd33b5b13d

--- aodhclient/osc.py
+++ aodhclient/osc.py
@@ -12,6 +12,8 @@
 # License for the specific language governing permissions and limitations
 # under the License.
 
+"""OpenStackClient plugin for Telemetry Alarming service."""
+
 from osc_lib import utils
 
 
